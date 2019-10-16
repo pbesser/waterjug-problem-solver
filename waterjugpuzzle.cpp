4fdece18ebc82628c3321d41951051dae280bbc1
pbesser <43225917+pbesser@users.noreply.github.com>
2019-10-15 23:42:37 -0400

    Update waterjugpuzzle.cpp

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -263,7 +263,7 @@ void solve(struct State currentCap, struct State finish){
     //makes visited array 
     bool** map = new bool*[currentCap.getA()];
     for(int i=0; i<currentCap.getA()+1;i++){
-        map[i]= new bool*[currentCap.getB()+1];
+        map[i]= new bool[currentCap.getB()+1];
         fill(map[i], map[i]+currentCap.getB(),false);
     }
     //call BFS for all the given peramaters
